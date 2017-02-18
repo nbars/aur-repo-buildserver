d0764d6af4a0bf4d824dcfd23c93a194ef11fcd3
Nils Bars <nils_bars@t-online.de>
2017-02-18 21:20:19 +0100

    Add test for RepoMovePackage and RepoDeltePackage

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,10 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR"
+
+#Is set to 1 if any assert fails
 exit_code=0
 
 export AUR_REPO_BUILDSERVER_TEST=true
-. ./../buildserver.sh --pkg-configs $DIR/configs --repo-dir $DIR/repo --action clean,build
+. ./../buildserver.sh --pkg-configs $DIR/configs --repo-dir $DIR/repo --action clean,build --debug
 
 function arrayEQ() {
   local arr0=( $1 )
@@ -57,17 +59,45 @@ function test_PkgX() {
   local pkg_path="$PWD/packages/xorg-server-common-1.19.1-5-x86_64.pkg.tar.xz"
 
   PkgGetName "$pkg_path"
-  assertEQ "$__result" "xorg-server-common"
+  assertEQ "$__result" "xorg-server-common" "$LINENO"
 
   PkgGetVersion "$pkg_path"
-  assertEQ "$__result" "1.19.1-5"
+  assertEQ "$__result" "1.19.1-5" "$LINENO"
 }
+
+function test_RepoX() {
+  rm -rf $DIR/repo/*
+  local err=0
+
+  local pkg_path="$PWD/packages/xorg-server-common-1.19.1-5-x86_64.pkg.tar.xz"
+  local pkg_copy_path="${pkg_path}_copy"
+  cp "$pkg_path" "$pkg_copy_path"
+
+  RepoMovePackage "$pkg_copy_path"
+
+  PkgGetName "$pkg_path"
+  local name="$__result"
+  assertEQ "$name" "xorg-server-common" "$LINENO"
+
+  RepoGetPackageVersion "$name"
+  assertEQ "$?" "$SUCCESS" "$LINENO"
+  assertEQ "$__result" "1.19.1-5" "$LINENO"
+
+  RepoRemovePackage "$name"
+
+  RepoGetPackageVersion "$name"
+  assertEQ "$?" "$ERROR" "$LINENO"
+}
+
+#Protect test files
 
 #Run tests
 test_config_parse
 test_cower
 test_PkgX
+test_RepoX
 
+rm -rf $DIR/repo/*
 CleanUp
 
 #Returns 1 if any test failed
